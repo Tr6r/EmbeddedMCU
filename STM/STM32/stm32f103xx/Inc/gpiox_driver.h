2a1eb51c5fa4a08c1385d9d61a4b4b2b818d2648
Đinh Quốc Cường <tr6r20@gmail.com>
2025-04-20 17:00:03 +0700

    add Input mode

--- STM/STM32/stm32f103xx/Inc/gpiox_driver.h
+++ STM/STM32/stm32f103xx/Inc/gpiox_driver.h
@@ -17,6 +17,9 @@ typedef enum {
 	GPIO_MODE_OUTPUT_2MHz = 0x10,
 	GPIO_MODE_OUTPUT_50MHz = 0x11,
 } GPIO_Mode_t;
+
+
+
 typedef enum {
 	GPIO_PIN_0 = 0,
 	GPIO_PIN_1 = 1,
@@ -39,7 +42,7 @@ typedef enum {
 typedef enum {
 	GPIO_CNF_INPUT_ANALOG = 0x00,      // Analog input
 	GPIO_CNF_INPUT_FLOATING = 0x01,    // Floating input
-	GPIO_CNF_INPUT_PU_PD = 0x02,
+	GPIO_CNF_INPUT_PU_PD = 0x10,
 	GPIO_CNF_OUTPUT_PP = 0x00,         // Push-Pull output
 	GPIO_CNF_OUTPUT_OD = 0x01,         // Open-Drain output
 	GPIO_CNF_AF_PP = 0x10,             // Alternate function Push-Pull output
@@ -49,6 +52,7 @@ typedef enum {
 typedef enum {
 	GPIO_PUSH_PULL = 0, GPIO_OPENDRAIN,
 } GPIO_Pull_t;
+
 
 typedef struct {
 	GPIO_Mode_t Mode;
@@ -62,7 +66,15 @@ typedef struct {
 	GPIO_TypeDef *Instance;
 } GPIO_Handle_t;
 
+
 GPIO_Handle_t GPIO_Init(GPIO_TypeDef *Instance, GPIO_Pin_t Pin,
 		GPIO_Mode_t Mode, GPIO_CNF_t CNF);
+
+
+
 void GPIO_WritePin(GPIO_Handle_t *xGPIO,int flag);
+
+
+
+
 #endif /* GPIOX_DRIVER_H_ */
--- STM/STM32/stm32f103xx/Src/gpio_driver.c
+++ STM/STM32/stm32f103xx/Src/gpio_driver.c
@@ -5,52 +5,82 @@
  *      Author: tr6r2
  */
 
-
 #include "gpiox_driver.h"
 
-GPIO_Handle_t GPIO_Init(GPIO_TypeDef* Instance, GPIO_Pin_t Pin, GPIO_Mode_t Mode, GPIO_CNF_t CNF)
-{
-    GPIO_Handle_t hGPIO;
-
-    // Cấu hình Instance và Pin
-    hGPIO.Instance = Instance;
-    hGPIO.GPIO_Config.Pin = Pin;
-    hGPIO.GPIO_Config.Mode = Mode;
-    hGPIO.GPIO_Config.CNF = CNF;
-
-    // Enable clock cho GPIO
-    if (Instance == GPIOA) GPIOA_EN_CLOCK();
-    else if (Instance == GPIOB) GPIOB_EN_CLOCK();
-    else if (Instance == GPIOC) GPIOC_EN_CLOCK();
-    else if (Instance == GPIOD) GPIOD_EN_CLOCK();
-    else if (Instance == GPIOE) GPIOE_EN_CLOCK();
-    else if (Instance == GPIOF) GPIOF_EN_CLOCK();
-    else if (Instance == GPIOG) GPIOG_EN_CLOCK();
+GPIO_Handle_t GPIO_Init(GPIO_TypeDef *Instance, GPIO_Pin_t Pin,
+		GPIO_Mode_t Mode, GPIO_CNF_t CNF) {
 
 
-    // Xác định chế độ (Mode) cho pin
-    if (Pin < 8) {
-           // Cấu hình CRL cho Pin 0-7
-           Instance->CRL &= ~(0xF << (Pin * 4));  // Clear previous settings
-           Instance->CRL |= (Mode << (Pin * 4)); // Set mode (2 bit)
-           Instance->CRL |= (CNF << (Pin * 4 + 2)); // Set CNF (2 bit)
+
+	GPIO_Handle_t hGPIO;
+
+	// Cấu hình Instance và Pin
+	hGPIO.Instance = Instance;
+	hGPIO.GPIO_Config.Pin = Pin;
+	hGPIO.GPIO_Config.Mode = Mode;
+	hGPIO.GPIO_Config.CNF = CNF;
+
+	// Enable clock cho GPIO
+	if (Instance == GPIOA)
+		GPIOA_EN_CLOCK();
+	else if (Instance == GPIOB)
+		GPIOB_EN_CLOCK();
+	else if (Instance == GPIOC)
+		GPIOC_EN_CLOCK();
+	else if (Instance == GPIOD)
+		GPIOD_EN_CLOCK();
+	else if (Instance == GPIOE)
+		GPIOE_EN_CLOCK();
+	else if (Instance == GPIOF)
+		GPIOF_EN_CLOCK();
+	else if (Instance == GPIOG)
+		GPIOG_EN_CLOCK();
+
+	if (Mode == GPIO_MODE_INPUT) {
+		uint8_t pinPos = (Pin % 8) * 4;
+
+		if (Pin < 8) {
+			Instance->CRL &= ~(0xF << pinPos);        // Clear config
+			Instance->CRL |=  (0x8 << pinPos);        // Input with pull-up/down
+			Instance->CRL &= ~(0b11 << 2);
+
+			// Set CNF0 = 10 (Input with pull-up / pull-down)
+			Instance->CRL |=  (0b10 << 2);
+			Instance->ODR |= (1 << 0);
+
+
+		} else {
+			Instance->CRH &= ~(0xF << pinPos);        // Clear config
+			Instance->CRH |=  (0x8 << pinPos);        // Input with pull-up/down
+	        Instance->CRH |= (CNF << ((Pin - 8) * 4 + 2)); // Set CNF (2 bit)
+
+		}
 	} else {
-           // Cấu hình CRH cho Pin 8-15
-           Instance->CRH &= ~(0xF << ((Pin - 8) * 4)); // Clear previous settings
-           Instance->CRH |= (Mode << ((Pin - 8) * 4)); // Set mode (2 bit)
-           Instance->CRH |= (CNF << ((Pin - 8) * 4 + 2)); // Set CNF (2 bit)
-       }
+		if (Pin < 8) {
+			// Cấu hình CRL cho Pin 0-7
+			Instance->CRL &= ~(0xF << (Pin * 4));  // Clear previous settings
 
+			Instance->CRL |= (Mode << (Pin * 4)); // Set mode (2 bit)
 
+			Instance->CRL |= (CNF << (Pin * 4 + 2)); // Set CNF (2 bit)
+		} else {
+			// Cấu hình CRH cho Pin 8-15
+			Instance->CRH &= ~(0xF << ((Pin - 8) * 4)); // Clear previous settings
+			Instance->CRH |= (Mode << ((Pin - 8) * 4)); // Set mode (2 bit)
+			Instance->CRH |= (CNF << ((Pin - 8) * 4 + 2)); // Set CNF (2 bit)
+		}
 
-    // Trả về GPIO Handle
-    return hGPIO;
+	}
+	// Xác định chế độ (Mode) cho pin
+
+	// Trả về GPIO Handle
+	return hGPIO;
 }
-void GPIO_WritePin(GPIO_Handle_t *xGPIO,int flag)
-{
-	if (flag == 1)	xGPIO->Instance->ODR &= ~(1 << xGPIO->GPIO_Config.Pin);  // Enable pull-up
-	else 			xGPIO->Instance->ODR |= (1 << xGPIO->GPIO_Config.Pin);  // Enable pull-up
-
+void GPIO_WritePin(GPIO_Handle_t *xGPIO, int flag) {
+	if (flag == 1)
+		xGPIO->Instance->ODR &= ~(1 << xGPIO->GPIO_Config.Pin); // Enable pull-up
+	else
+		xGPIO->Instance->ODR |= (1 << xGPIO->GPIO_Config.Pin); // Enable pull-up
 
 }
 
