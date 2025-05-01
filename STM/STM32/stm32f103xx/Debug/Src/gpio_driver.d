438725469f4deb518999d2cda2dcf3a2b5ab759f
Đinh Quốc Cường <tr6r20@gmail.com>
2025-05-01 17:01:00 +0700

    Remove Debug folder from tracking

--- STM/STM32/stm32f103xx/Debug/Src/gpio_driver.d
+++
@@ -1,4 +0,0 @@
-Src/gpio_driver.o: ../Src/gpio_driver.c ../Inc/gpiox_driver.h \
- ../Inc/stm32f103xx.h
-../Inc/gpiox_driver.h:
-../Inc/stm32f103xx.h:
--- STM/STM32/stm32f103xx/Debug/Src/main.d
+++
@@ -1,5 +0,0 @@
-Src/main.o: ../Src/main.c ../Inc/stm32f103xx.h ../Inc/gpiox_driver.h \
- ../Inc/stm32f103xx.h
-../Inc/stm32f103xx.h:
-../Inc/gpiox_driver.h:
-../Inc/stm32f103xx.h:
