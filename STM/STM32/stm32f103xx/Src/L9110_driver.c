9c34a6f6aba64c00cb5a31fd5c8a81508417687b
Đinh Quốc Cường <tr6r20@gmail.com>
2025-05-17 19:14:13 +0700

    DEBUG L9110_Straight_Dis()

--- STM/STM32/stm32f103xx/Src/L9110_driver.c
+++ STM/STM32/stm32f103xx/Src/L9110_driver.c
@@ -41,35 +41,36 @@ void L9110_Stop(L9110_Handle_t *Instance) {
 	}
 }
 
-//void L9110_Straight_Dis(L9110_Handle_t *Instance, L9110_Direction_t Dir,
-//		uint16_t pwm, volatile int32_t *encoder_total) {
-//	// Chạy motor theo hướng Dir
-//	if (Dir == L9110_DIRECTION_FORWARD) {
-//		GPIO_WritePin(Instance->Dir_Pin, GPIO_HIGH);  // hoặc LOW tùy mạch
-//	} else if (Dir == L9110_DIRECTION_REVERSE) {
-//		GPIO_WritePin(Instance->Dir_Pin, GPIO_LOW);   // hoặc HIGH tùy mạch
-//	}
-//
-//	// Bắt đầu cấp PWM
-//	switch (Instance->hTIM->Config.Channel) {
-//	case TIM_CHANNEL_1:
-//		Instance->hTIM->Instance->CCR1 = pwm;
-//		break;
-//	case TIM_CHANNEL_2:
-//		Instance->hTIM->Instance->CCR2 = pwm;
-//		break;
-//	case TIM_CHANNEL_3:
-//		Instance->hTIM->Instance->CCR3 = pwm;
-//		break;
-//	case TIM_CHANNEL_4:
-//		Instance->hTIM->Instance->CCR4 = pwm;
-//		break;
-//	}
-//
-//	// Đợi đến khi encoder_total đạt 666
-//	while (*encoder_total < 666)
-//		;
-//
-//	// Sau khi đạt 666, dừng động cơ
-//	L9110_Stop(Instance);
-//}
+void L9110_Straight_Dis(L9110_Handle_t *Instance, L9110_Direction_t Dir,
+		uint16_t pwm,TIMER2_5_TypeDef_t *EncInstance, uint16_t Enc) {
+	Instance->hTIM->Instance->CNT = 0;
+	EncInstance->CNT = 0;
+	if (Dir == L9110_DIRECTION_FORWARD) {
+		if (Instance->hTIM->Config.Channel == TIM_CHANNEL_1) {
+			Instance->hTIM->Instance->CCR1 = pwm;  // PWM ra kênh 1
+			Instance->hTIM->Instance->CCR2 = 0;    // Kênh 2 tắt
+		} else if (Instance->hTIM->Config.Channel == TIM_CHANNEL_3) {
+			Instance->hTIM->Instance->CCR3 = pwm;  // PWM ra kênh 1
+			Instance->hTIM->Instance->CCR4 = 0;    // Kênh 2 tắt
+		}
+
+	} else if (Dir == L9110_DIRECTION_REVERSE) {
+		if (Instance->hTIM->Config.Channel == TIM_CHANNEL_1) {
+			Instance->hTIM->Instance->CCR1 = 0;  // PWM ra kênh 1
+			Instance->hTIM->Instance->CCR2 = pwm;    // Kênh 2 tắt
+		} else if (Instance->hTIM->Config.Channel == TIM_CHANNEL_3) {
+			Instance->hTIM->Instance->CCR3 = 0;  // PWM ra kênh 1
+			Instance->hTIM->Instance->CCR4 = pwm;    // Kênh 2 tắt
+		}
+	}
+	// Đợi đến khi encoder_total đạt 666
+
+	while (abs((int16_t)(EncInstance->CNT)) < Enc);
+	// Sau khi đạt 666, dừng động cơ
+
+	L9110_Straight(Instance, Dir == L9110_DIRECTION_FORWARD ? L9110_DIRECTION_REVERSE :L9110_DIRECTION_FORWARD, 90);  // PWM 80%
+	for (volatile int i = 0; i < 50000; i++);  // Delay nhỏ, có thể tăng giảm
+
+	L9110_Stop(Instance);
+
+}
